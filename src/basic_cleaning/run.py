421e3d3599f3535b4a7cdcbdeb5a70ea85842261
Kevin McGee <kevinpmcgee14@gmail.com>
2021-11-24 09:54:23 -0600

    finished pipeline.

---
+++ src/basic_cleaning/run.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+"""
+Download from W&B the raw dataset and apply some basic data cleaning, exporting the result to a new artifact
+"""
+import argparse
+import logging
+import wandb
+import pandas as pd
+
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
+logger = logging.getLogger()
+
+
+def go(args):
+
+    run = wandb.init(job_type="basic_cleaning")
+    run.config.update(args)
+
+    # Download input artifact. This will also log that this script is using this
+    # particular version of the artifact
+    # artifact_local_path = run.use_artifact(args.input_artifact).file()
+
+    logger.info("Getting artifact...")
+    input_data = run.use_artifact(args.input_artifact)
+    filepath = input_data.file()
+    df = pd.read_csv(filepath)
+    
+    logger.info("Artifact successfully retrieved and opened. Starting data cleaning...")
+    idx = df['price'].between(args.min_price, args.max_price)
+    df = df[idx].copy()
+    df['last_review'] = pd.to_datetime(df['last_review'])
+    df.to_csv('clean_sample.csv', index=False)
+    
+    logger.info("Data cleaning successful. Adding artifact to W&B...")
+    artifact = wandb.Artifact(
+     args.output_artifact,
+     type=args.output_type,
+     description=args.output_description,
+    )
+    artifact.add_file("clean_sample.csv")
+    run.log_artifact(artifact)
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description="A very basic data cleaning")
+
+
+    parser.add_argument(
+        "--input_artifact", 
+        type=str,
+        help='The name of the W&B Artifact that we will read in for cleaning',
+        required=True
+    )
+
+    parser.add_argument(
+        "--output_artifact", 
+        type=str,
+        help='The name of the W&B Artifact that we will write out after cleaning',
+        required=True
+    )
+
+    parser.add_argument(
+        "--output_type", 
+        type=str,
+        help="The type of the W&B Artifact",
+        required=True
+    )
+
+    parser.add_argument(
+        "--output_description", 
+        type=str,
+        help="The description of the W&B Artifact",
+        required=True
+    )
+
+    parser.add_argument(
+        "--min_price", 
+        type=float,
+        help="The minimum price that will be used to drop outliers",
+        required=True
+    )
+
+    parser.add_argument(
+        "--max_price", 
+        type=float,
+        help="The maximum price that will be used to drop outliers",
+        required=True
+    )
+
+
+    args = parser.parse_args()
+
+    go(args)
--- src/data_check/test_data.py
+++ src/data_check/test_data.py
@@ -63,3 +63,13 @@ def test_similar_neigh_distrib(data: pd.DataFrame, ref_data: pd.DataFrame, kl_th
 ########################################################
 # Implement here test_row_count and test_price_range   #
 ########################################################
+
+def test_row_count(data):
+    """
+    Check to make sure the number of rows falls between the expected range (15000, 1000000)
+    """
+    assert 15000 < data.shape[0] < 1000000
+
+
+def test_price_range(data, min_price, max_price):
+    assert all(data['price'].between(min_price, max_price))
